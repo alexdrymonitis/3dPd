1068602184fb74c81535a706e811d097d18bb120
Alexandros Drymonitis <alexdrymonitis@gmail.com>
2017-07-01 17:29:46 +0300

    Update modular_synthesizer.ino

--- Arduino_code/modular_synthesizer/modular_synthesizer.ino
+++ Arduino_code/modular_synthesizer/modular_synthesizer.ino
@@ -1,12 +1,12 @@
-/*****************************************************************************
- * This Arduino sketch is written for a physical digital modular synthesizer *
- * The control part of this synthesizer runs on a Teensy3.2 (or 3.5, 3.6)    *
- * which is programmed in the Arduino language using Teensyduino             *
- * the audio part of the synthesizer runs on an embedded computer (Odroid-U3 *
- * or a Raspberry Pi 3), which is programmed in Pure Data. In order for this *
- * code to run, you'll need to combine it with the Pd patch that comes with  *
- * this sketch.                                                              *
- *****************************************************************************/
+/***********************************************************************************
+ * This Arduino sketch is written for a physical digital modular synthesizer       *
+ * The control part of this synthesizer runs on a Teensy3.2 (or 3.5, 3.6)          *
+ * which is programmed in the Arduino language using Teensyduino                   *
+ * the audio part of the synthesizer runs on an embedded computer (Raspberry Pi 3) *
+ * which is programmed in Pure Data. In order for this                             *
+ * code to run, you'll need to combine it with the Pd patch that comes with        *
+ * this sketch.                                                                    *
+ ***********************************************************************************/
 
 // inculde the SPI library to control the shift registers
 #include <SPI.h>
